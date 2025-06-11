9039c78de325edb3c386d7a2bce563ab27cb6b15
FlameOregu <blueoregu@gmail.com>
2025-06-12 00:10:30 +0100

    Combat UI Start
    Added a bunch of assets and images like the background, different buttons. Fixed some of the code so it no longer crashes when changing scene.

--- Scenes/battle_behaviour.gd
+++ Scenes/battle_behaviour.gd
@@ -1,14 +1,16 @@
 extends Node2D
 var is_moving:bool = false
 var exited:bool = false
-
+var timer:bool = false
+var battlestarted:bool = false
 
 func _on_battle_area_area_entered(area:Area2D):
 	exited = false
 	print("entered area")
 	while exited == false:
 		_battle_roller()
-		await get_tree().create_timer(1.0).timeout
+		if battlestarted == false:
+			await get_tree().create_timer(1.0).timeout
 
 func _on_battle_area_area_exited(area:Area2D):
 	exited = true
@@ -19,4 +21,6 @@ func character_is_moving(_is_moving:bool):
 func _battle_roller():
 	if is_moving == true:
 		if randi_range(1,6) == 6:
+			battlestarted = true
 			print("Battle Commence")
+			get_tree().change_scene_to_file("res://Scenes/BattleScene.tscn")
