fb9745ec4e060fe5d5bea9a6a26f31701998377c
FlameOregu <blueoregu@gmail.com>
2025-07-02 21:07:33 +0100

    UI Navigation fix
    Re-made the fight icons because of some weird overlap issue, added proper ui navigation that works with other panels and added a text panel for a basic test.

---
+++ Scenes/action_1.gd
@@ -0,0 +1,14 @@
+extends Button
+signal on_cancel
+var focus = false
+
+func _on_select_fight():
+	self.grab_focus()
+
+func _process(delta):
+	if Input.is_action_just_pressed("cancel") == true and focus == true:
+		on_cancel.emit()
+		focus = false
+
+func _on_focus_entered():
+	focus = true
--- Scenes/battle_controller.gd
+++ Scenes/battle_controller.gd
@@ -7,6 +7,6 @@ func _on_area_entered(area:Area2D):
 func _on_area_exited(area:Area2D):
 	print("area_left")
 
-func _on_character_on_character_moving(is_moving:bool):
+func _on_character_moving(is_moving:bool):
 	$BattleBehaviour.character_is_moving(is_moving)
 	
