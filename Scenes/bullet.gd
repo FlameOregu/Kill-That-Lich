84bd3e91deaf146078a5350054ceabd069a7bfa7
FlameOregu <blueoregu@gmail.com>
2025-07-15 21:39:55 +0100

    boolet
    made bullet spawner. i hate this

--- Scenes/bullet.gd
+++ Scenes/bullet.gd
@@ -1,15 +1,18 @@
-extends Area2D
+extends Node2D
 
-var velocity: Vector2 = Vector2()
+var velocity: float = 50
 var duration = 20
 
-func _process(delta: float) -> void:
-	position += velocity * delta
+var move_dir: Vector2
+
+func _process(delta):
+	translate(move_dir * velocity * delta)
 	duration -= delta
-	if duration <= 0:
+	if duration < 0:
 		queue_free()
-		
+		print("deleted")
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "hurtbox":
 		queue_free()
+		print("deleted")
--- enemy.gd
+++ enemy.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+var bullet_scene : PackedScene = preload("res://Scenes/bullet.tscn")
+
+@export var shoot_rate : float = 0.5
+var last_shoot_time : float
+
+func _process(delta: float) -> void:
+	if Time.get_unix_time_from_system() - last_shoot_time > shoot_rate:
+		_shoot()
+
+func _shoot():
+	last_shoot_time = Time.get_unix_time_from_system()
+	var bullet = bullet_scene.instantiate()
+	self.add_child(bullet)
+	bullet.global_position = $"Bullet Spawner".global_position
+	bullet.move_dir = Vector2(1,0)
+	print("bullet spawned")
