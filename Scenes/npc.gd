5576e579d9d7ee11f111c3c675bdce9f94e3d5d7
FlameOregu <blueoregu@gmail.com>
2025-08-12 22:32:37 +0100

    added text
    added text

---
+++ Scenes/npc.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+signal start_dialogue
+var intext : bool
+var textboxgb
+var textbox_scene : PackedScene = preload("res://Scenes/textbox.tscn")
+
+func _on_start_dialogue() -> void:
+	var textbox = textbox_scene.instantiate()
+	textboxgb = textbox
+	$"../Text Layer".add_child(textbox)
+	var viewport_size = get_viewport_rect().size
+	var size = textbox.get_size()
+	textbox.position.x = viewport_size.x / 2 - size.x / 2
+	textbox.position.y = viewport_size.y - size.y - 10
+	var label = textbox.get_node("Text")
+	if self == $"../NPC Test2":
+		label.text = "what do you want nerd"
+	else:
+		label.text = "sup"
+	textbox.visible = true
+	$"../Character".inmenu = true
+	await get_tree().create_timer(0.2).timeout
+	intext = true
+
+func _input(event):
+	if Input.is_action_just_pressed("interact") and intext == true:
+		textboxgb.queue_free()
+		intext = false
+		$"../Character".inmenu = false
+		$"../Character".speed = 100
--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 @export var speed = 100
 var direction:Vector2
-
+var interactrad : bool
+var currentnpc : Node2D
+var inmenu = false
 signal on_character_moving(is_moving:bool)
+
 func _process(_delta):
 	direction = Input.get_vector("left","right","up","down") 
 	if !direction.is_zero_approx():
@@ -14,3 +17,21 @@ func _physics_process(_delta):
 	velocity = direction * speed
 	move_and_slide()
 	
+func _input(event):
+	if Input.is_action_just_pressed("interact") and interactrad == true and inmenu == false: #checks if in menu and in interaction radius
+		var npcs : Array = get_tree().get_nodes_in_group("NPC") #grabs all nodes in the NPC group and puts in array
+		var shortest_distance : float = global_position.distance_to(npcs[0].global_position)
+		currentnpc = npcs[0]
+		for i in npcs:
+			var currentdist : float = global_position.distance_to(i.global_position)
+			if currentdist < shortest_distance:
+				currentnpc = i
+				shortest_distance = currentdist
+		speed = 0
+		currentnpc.emit_signal("start_dialogue") #tells the npc to go start the damn dialogue
+
+
+func _on_interaction_area_entered(area: Area2D) -> void:
+	interactrad = true
+func _on_interaction_area_exited(area: Area2D) -> void:
+	interactrad = false
