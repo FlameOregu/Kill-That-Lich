a1e3876d82f0c5b36c7d849b719c32f9a662be4d
FlameOregu <blueoregu@gmail.com>
2026-05-02 21:57:09 +0100

    stats panel progress
    started on it, did a bit, added the dialogue addon, need to create a dialogue bubble in the new addon

--- Scripts/game_menu_logic.gd
+++ Scripts/game_menu_logic.gd
@@ -1,18 +1,20 @@
 extends Node
 @export var skillpanel : Control
 @export var invpanel : Control
+@export var statspanel : Control
 @export var skillstab : Control
 @export var invtab : Control
+@export var statstab : Control
 var inmenu = false
 signal closemenu
 
 func _showskills():
 	skillpanel.show()
-	invpanel.hide()
+	statspanel.hide()
 	skillstab.size = Vector2(144,26)
 	skillstab.position = Vector2(21, 4)
-	invtab.size = Vector2(144,18)
-	invtab.position = Vector2(165, 12)
+	statstab.size = Vector2(144,18)
+	statstab.position = Vector2(309, 12)
 
 func _showinv():
 	invpanel.show()
@@ -22,6 +24,14 @@ func _showinv():
 	skillstab.size = Vector2(144,18)
 	skillstab.position = Vector2(21, 12)
 
+func _showstats():
+	statspanel.show()
+	invpanel.hide()
+	statstab.size = Vector2(144,26)
+	statstab.position = Vector2(309, 4)
+	invtab.size = Vector2(144,18)
+	invtab.position = Vector2(165,12)
+	
 func _openmenu():
 	_showskills()
 	inmenu = true
@@ -33,8 +43,11 @@ func _input(event):
 				_showinv() #shows inventory tab
 				$"../Inventory Panel/Inventory Container".get_child(0).grab_focus()
 			elif invpanel.visible:
-				_showskills() #shows skill tab
+				_showstats() #shows skill tab
 				$"../Inventory Panel/Inventory Container".get_child(0).release_focus()
+			elif statspanel.visible:
+				_showskills()
+				
 		elif Input.is_action_just_pressed("cancel"):
 			inmenu = false
 			closemenu.emit()
